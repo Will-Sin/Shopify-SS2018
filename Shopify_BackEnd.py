a4bf5641311776201c0ac6d06a57844c4e1db7c0
Willem Deisinger <33158878+Will-Sin@users.noreply.github.com>
2018-01-04 18:18:01 -0500

    Update Shopify_BackEnd.py
    Added PPrint function
    Added page parsing
    Edited menus parsing to work with multiple pages

--- Shopify_BackEnd.py
+++ Shopify_BackEnd.py
@@ -1,25 +1,43 @@
 import json
 from urllib.request import urlopen
+import pprint
 
-html = urlopen('https://backend-challenge-summer-2018.herokuapp.com/challenges.json?id=1&page=1')
-
-data = html.read()
-decoded = data.decode('utf-8')
-
-JSON = json.loads(decoded)
-
+'''
+Parse through all pages and place pages JSON into a list. Continue through all pages until the page has no data
+'''
+JSON_pages = []
+y = 1
+while True:
+     html = urlopen('https://backend-challenge-summer-2018.herokuapp.com/challenges.json?id=1&page=' + str(y))
+     y += 1
+     data = html.read()
+     decoded = data.decode('utf-8')
+     JSON = json.loads(data.decode('utf-8'))
+     if len(JSON['menus']) == 0:
+          break
+     else:
+          JSON_pages.append(JSON)
+     
+     
 '''
 Parse through captured JSON data and place each node into the list Menus
 '''
 Menus = []
-x = True
-y = 0
-while x == True:
-     try:
-          Menus.append(JSON['menus'][int(y)])
-          y += 1
-     except IndexError:
-          x = False
+for x in range(0, len(JSON_pages)):
+     z = True
+     y = 0
+     Menus.append([])
+     while z == True:
+          try:
+               Menus[int(x)].append(JSON_pages[int(x)]['menus'][int(y)])
+               y += 1
+          except IndexError:
+               z = False
 
 
-test = {'menus':}
+test = {'menus': [] }
+
+def p_print(data):
+     pp = pprint.PrettyPrinter(indent=4)
+     pp.pprint(data)
+     
